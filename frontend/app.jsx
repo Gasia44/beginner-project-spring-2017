6fdda16b0f5d94dc92ec915e786f7ac9a396b27f
Gasia Atashian <gasiaatashian@gmail.com>
2017-04-08 14:21:53 +0400

    Router

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Link, Route, BrowserRouter as R} from 'react-router-dom';
 
 import Layout from './Layout';
 
-render(
-    <Layout/>, document.getElementById('container'));
+const HomePage= () =>(
+  <div>
+    Home page
+  </div>
+);
+
+
+const App = () =>{
+  return(
+    <R>
+    <div>
+      <Route exact path={'/'} component={HomePage}/>
+      <Route path={'/Piano'} component={Layout}/>
+
+      <p><Link to={'/Piano'}> Piano </Link></p>
+      <p><Link to={'/Drum'}> Drum </Link></p>
+      <p><Link to={'/Guitar'}> Guitar </Link></p>
+
+    </div>
+  </R>
+);
+};
+
+render(<App/>, document.getElementById('container'));
